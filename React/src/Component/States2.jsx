24cd952a24b81af61b99d8081691b154e5f41806
anantkpatel <imagine231216@gmail.com>
2025-09-20 18:58:10 +0530

    lk

--- React/src/Component/States2.jsx
+++ React/src/Component/States2.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 function States2() {
   // const [val,setVal]=React.useState({name:"anant",age :24});
 
-  
+
   // const [val,setVal]=useState(["anant","ajit","deepak","dileep","ankesh","monish","Abhisek","Anoop"]);
 
   const [val,setVal]=useState([
     {name:"anant",age:24},
     {name:"ajit",age:25},
-    {name:"deepak",age:26},
+    {name:"deepak",age:26}
   ])
   return (
     <>
@@ -21,11 +21,21 @@ function States2() {
     
 
     <div className="p-5 bg-green-300">
-      {val.map((elem=> <h1>{elem}</h1>))}
+      {/* {val.map((elem=> <h1>{elem}</h1>))} */}
+      {val.map((elem,index)=>{
+        <div>
+          <h1>elem</h1>
+          <h1 >{elem.name}</h1>
+          <h2 >{elem.age}</h2> 
+
+
+        </div>
+      })}
+
     </div>
-    <button onClick={()=>setVal(()=>{
+    {/* <button onClick={()=>setVal(()=>{
       return val.filter((item,index)=> index!=val.length-1)
-    })} className="p-2 m-2 bg-blue-600 rounded-full text-white">Remove</button>
+    })} className="p-2 m-2 bg-blue-600 rounded-full text-white">Remove</button> */}
     {/* dekho  filter kya karege vo ke naya array return k  rega jisme se humne last element ko hata diya hai or vo setVal ke ander set kar diya hai
     ithi button pe click karne se last element remove ho jayega
      */}   
