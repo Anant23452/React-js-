502e93ac904017706a493ac86b7f7389c7424be0
anantkpatel <imagine231216@gmail.com>
2025-09-21 18:54:19 +0530

    handle click

--- React/src/Component/PropsPractice.jsx
+++ React/src/Component/PropsPractice.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function PropsPractice(image,course,name) {
+function PropsPractice({image,course,name}) {
  
   return (
     <div className='w-60 bg-zinc-200'>
-      <div className="w-full h-32 bg-sky-400">
-        <img src={image} alt="nothing" />
+      <div className="w-full h-40 bg-sky-400">
+        <img src={image} alt="nothing" className='w-full h-full object-cover' />
       </div>
         <div className="w-full p-3 text-black">
           <h3>{name}</h3>
           <h3>{course} </h3>
-          <button className="mt-3 bg-blue-700 text-white">Click</button>
+          <button onClick={()=>alert("Adding as a friend")} className="mt-3 bg-blue-700 text-white">Click</button>
         </div>
       
     </div>
