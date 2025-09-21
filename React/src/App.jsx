a8abc389a873bc64b7b5d3a2c02c254443e67a51
anantkpatel <imagine231216@gmail.com>
2025-09-21 18:40:59 +0530

    I am placing dynamic images

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -15,6 +15,14 @@ import PropsPractice from "./Component/PropsPractice";
 //make function name in capital letter
 function App(){
   // const arr=["anant","ajit","deepak","dileep","ankesh","monish","Abhisek","Anoop"];
+
+  //props ke liye hai 
+   const app=[
+    {name:"Anant",course:"MCA",image:"https://images.unsplash.com/photo-1758380742154-44738eb92832?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Ajit",course:"BCA",image:"https://i.pinimg.com/736x/c8/96/dc/c896dc6b5f32dc59a75d4f8354e518db.jpg"},
+    {name:"Deepak",course:"B.Tech",image:"https://i.pinimg.com/736x/dd/cd/70/ddcd70b0982c7818456f8a62dfe740c8.jpg"},
+    {name:"Dileep",course:"MBA",image:"https://i.pinimg.com/736x/7a/46/c9/7a46c93d08199b4fd4a8dd5f17f1dc5a.jpg"},
+  ]
   return (
     <>
     {/* <div>
@@ -31,7 +39,12 @@ function App(){
     {/* < ImageCard/> */}
     {/* <Props chacha="Know more" color="bg-blue-600"  />
     <Props chacha="Download" color="bg-red-600" /> */}
-    <PropsPractice/>
+    <div className="w-screen h-screen bg-zinc-400 flex gap-4 items-center justify-center">
+
+    {app.map((elem,index)=>(
+      <PropsPractice image={elem.image} course ={elem.course} name={elem.name} />
+    ))}
+    </div>
     
     </>
   );
--- React/src/Component/PropsPractice.jsx
+++ React/src/Component/PropsPractice.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function PropsPractice() {
+function PropsPractice(image,course,name) {
+ 
   return (
-    <div>PropsPractice</div>
+    <div className='w-60 bg-zinc-200'>
+      <div className="w-full h-32 bg-sky-400">
+        <img src={image} alt="nothing" />
+      </div>
+        <div className="w-full p-3 text-black">
+          <h3>{name}</h3>
+          <h3>{course} </h3>
+          <button className="mt-3 bg-blue-700 text-white">Click</button>
+        </div>
+      
+    </div>
   )
 }
 
