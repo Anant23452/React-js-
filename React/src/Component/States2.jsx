1fc9da141d3df2ebfc9aa883f66e1414b355cf18
anantkpatel <imagine231216@gmail.com>
2025-09-21 10:32:59 +0530

    fd

--- React/src/Component/States2.jsx
+++ React/src/Component/States2.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 function States2() {
-  // const [val,setVal]=React.useState({name:"anant",age :24});
+  // const [val,setVal]=useState({name:"anant",age :24});
 
 
   // const [val,setVal]=useState(["anant","ajit","deepak","dileep","ankesh","monish","Abhisek","Anoop"]);
@@ -15,27 +15,22 @@ function States2() {
     <>
     {/* //use state state ko turant update nahi karta ,vo usestate state ko update karta hai apne hisaab se function completion ke badd to fix performace kssues */}
     {/* <button onClick={()=>{
-      seVal({...val,gender:"male"});
+      setVal({...val,gender:"male"});
       console.log(val);
     }}></button> */}
     
 
     <div className="p-5 bg-green-300">
       {/* {val.map((elem=> <h1>{elem}</h1>))} */}
-      {val.map((elem,index)=>{
-        <div>
-          <h1>elem</h1>
+      {val.map((elem)=>(
+        <div key={elem.name}>
           <h1 >{elem.name}</h1>
           <h2 >{elem.age}</h2> 
-
-
         </div>
-      })}
+      ))}
 
     </div>
-    {/* <button onClick={()=>setVal(()=>{
-      return val.filter((item,index)=> index!=val.length-1)
-    })} className="p-2 m-2 bg-blue-600 rounded-full text-white">Remove</button> */}
+    <button onClick={()=>{setVal(()=>val.map(item=>item.name==="ajit"?({name:"ajit",age:26}):item))}} className="p-2 m-2 bg-blue-600 rounded-full text-white">Remove</button>
     {/* dekho  filter kya karege vo ke naya array return k  rega jisme se humne last element ko hata diya hai or vo setVal ke ander set kar diya hai
     ithi button pe click karne se last element remove ho jayega
      */}   
