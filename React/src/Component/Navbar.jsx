297e0c6c19f696488ad4f28a01694702851ca826
anantkpatel <imagine231216@gmail.com>
2025-09-23 18:43:35 +0530

    chagneing bg

--- React/src/Component/Navbar.jsx
+++ React/src/Component/Navbar.jsx
@@ -3,10 +3,10 @@ import style from './style.module.css'
 function Navbar({data,index}){
     return(
         <div className="w-full py-3 px-4 flex justify-between items-center">
-            <h1>Orange</h1>
+            <h1 className={style.a} >Orange</h1>
             <div className="flex p-2 px-4 bg-orange-600 rounded-full gap-4 text-black">
                 <h3>Favorites</h3>
-                <h4>
+                <h4 >
                     {/* {index} */}
                     {data.filter((item)=>item.added).length}
                     </h4>
