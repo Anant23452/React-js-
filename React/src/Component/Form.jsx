512e468fd8ad819b5f67b04f8b2d3c8e22dfe128
anantkpatel <imagine231216@gmail.com>
2025-09-23 19:13:23 +0530

    form submition

--- React/src/Component/Form.jsx
+++ React/src/Component/Form.jsx
@@ -4,6 +4,20 @@ import React from 'react'
 //To uske liye hume ek function banana padta hai jisme event ko pass karte hai
 //Aur fir event.preventDefault() kar dete hai
 
+//method
+//1. Controlled Component
+//2. Uncontrolled Component
+//3.REact Hook Form
+//4.useRef Hook
+
+//useRef = is tareeke me hum har input ko select kar lete hai and unki value tab nikalte hai jab form submit hota hai
+
+
+//Controlled Component
+//Jab hum form ke input ko react state ke through control karte hai to use controlled component kehte hai
+//Yaha pe hum har ek input field ke liye ek state banate hai aur uske value ko state se bind karte hai
+//Aur jab bhi input change hota hai to hum state ko update karte hai
+
 function Form() {
   return (
     <div className='bg-zinc-300 py-5 px-3'>
