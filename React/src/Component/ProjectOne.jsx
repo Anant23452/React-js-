e5430536dcdfb4d27ce28c0b16413b8b235b97a1
anantkpatel <imagine231216@gmail.com>
2025-09-23 09:15:18 +0530

    project making

--- React/src/Component/ProjectOne.jsx
+++ React/src/Component/ProjectOne.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function ProjectOne() {
   return (
-    <div>ProjectOne</div>
+    <div className="w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-10 relative">
+      <div className="w-20 h-20 bg-orange-500 rounded-md"> </div>
+        <div className="text-black">
+          <h3 className='text-xl leading-none font-semibold'>Song name</h3>
+          <h5>Artist name</h5>
+        </div>
+        <button className="px-3 py-2 bg-blue-600 text-white-100 rounded-full text-xs absolute bottom-0 -translate-y-[-50%]">Click to Favorite</button>
+    </div>
   )
 }
 
