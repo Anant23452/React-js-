9bfdaec0b753f76abd32b204b7f2fb1673c42ad0
anantkpatel <imagine231216@gmail.com>
2025-09-25 08:13:59 +0530

    form

--- React/src/Component/Form.jsx
+++ React/src/Component/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 //Agar kabhi bhi form submit karte ho to form automatically refresh ho jata hai
 //To uske liye hume ek function banana padta hai jisme event ko pass karte hai
@@ -19,12 +19,16 @@ import React from 'react'
 //Aur jab bhi input change hota hai to hum state ko update karte hai
 
 function Form() {
+ const name = useRef(null);
+ const age = useRef(null);
   return (
     <div className='bg-zinc-300 py-5 px-3 flex flex-col gap-3 rounded-md'>
+      <form action="" onSubmit={(event)=>event.preventDefault()}>
         <input type="Name" placeholder='name' />
-        <input type="Name" placeholder='adress' />
+        <input type="Name" placeholder='age' />
         <input type="Name" placeholder='email' />
         <button>Submit</button>
+        </form>
     </div>
   )
 }
