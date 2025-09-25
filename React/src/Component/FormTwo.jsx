9d84455f37210a2e508cc8e8c0954528503654af
anantkpatel <imagine231216@gmail.com>
2025-09-25 09:15:34 +0530

    controll componet

--- React/src/Component/FormTwo.jsx
+++ React/src/Component/FormTwo.jsx
@@ -8,8 +8,8 @@ function FormTwo() {
     }
   return (
     <form action="" onSubmit={handleSubmit} className='flex flex-col justify-center items-center bg-zinc-500'>
-       < input onChange={(event)=>setVal({name:event.target.value})} type="text" placeholder='Enter your name' className='m-3 p-2 rounded-md'/>
-         < input onChange={(event)=>setVal({email:target.value})} type="email" placeholder='Enter your email' className='m-3 p-2 rounded-md'/>
+       < input onChange={(event)=>setVal({...val,name:event.target.value})} type="text" placeholder='Enter your name' className='m-3 p-2 rounded-md'/>
+         < input onChange={(event)=>setVal({...val,email:event.target.value})} type="email" placeholder='Enter your email' className='m-3 p-2 rounded-md'/>
        < button  className='bg-blue-600 text-white px-3 py-2 rounded-md m-3'>Submit</button>
     </form>
   )
