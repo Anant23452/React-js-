ea75325140264a2a0f50c85eeccc337187c56558
anantkpatel <imagine231216@gmail.com>
2025-09-26 09:25:32 +0530

    kjl

--- React/src/Component/Boxes.jsx
+++ React/src/Component/Boxes.jsx
@@ -3,7 +3,7 @@ import Box from "./Box";
 
 function Boxes() {
   return (
-    <div className='w-full max-h-72 overflow-auto bg-blue-500 p-5 flex gap-4 justify-center flex-wrap '>
+    <div className='w-full max-h-72 overflow-auto  p-5 flex gap-4 justify-center flex-wrap '>
         <Box/>
       
     </div>
--- React/src/Component/Page.jsx
+++ React/src/Component/Page.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 function Page () {
   return (
-    <div className=' mt-10 flex justify-center bg-green-800  '>
-      <form action=""className='flex gap-10 flex-col ' >
-          <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
-        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" />
+    <div className=' mt-10 flex justify-center   '>
+      <form action=""className='flex gap-5 flex-col ' >
+          <input className='rounded-md px-2 py-1 text-base font-semibold outline-none  type="text"' placeholder='Name' />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='email' />
+        <input className='rounded-md px-2 py-1 text-base font-semibold outline-none' type="text" placeholder='web' />
+        <input className='bg-red-400' type="button" value="Submit" />
       </form>
     </div>
   )
