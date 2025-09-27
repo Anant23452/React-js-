219091539a0701468ae02864c25ac43052412dcc
anantkpatel <imagine231216@gmail.com>
2025-09-27 17:30:31 +0530

    lk

--- ReactRout/my-app/Components/User.jsx
+++ ReactRout/my-app/Components/User.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function User() {
+  const [First,setFirst]=useState("This is normal data");
+  const [Second,setSecond]=useState("This is Big data");
+
+  useEffect(()=>{
+   console.log("Going on user page");
+
+   return()=>{
+    console.log("Leaving user page");
+   }
+  })
   return (
     <div>User</div>
   )
