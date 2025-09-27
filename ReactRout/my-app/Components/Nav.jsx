dbfd9bc204f813bf72e9cec39367937b6132f3c4
anantkpatel <imagine231216@gmail.com>
2025-09-27 15:17:26 +0530

    fh

--- ReactRout/my-app/Components/Nav.jsx
+++ ReactRout/my-app/Components/Nav.jsx
@@ -5,9 +5,24 @@ function Nav() {
   return (
     <div>
           <nav className="mt-10 flex justify-around bg-black text-white p-5">
-      <NavLink style={(e)=>{console.log(e)}}  to="/">Home</NavLink>
-      <NavLink style={()=>{}} to="/user">User</NavLink>
-      <NavLink style={()=>{}} to="/about">About</NavLink>
+      <NavLink style={(e)=>{
+        return{
+          color:e.isActive?"red":"white",
+           fontWeight:e.isActive?"bold":"normal"
+        }
+      }}  to="/">Home</NavLink>
+      <NavLink style={(e)=>{
+          return{
+          color:e.isActive?"red":"white",
+          fontWeight:e.isActive?"bold":"normal"
+        }
+      }} to="/user">User</NavLink>
+      <NavLink style={(e)=>{
+          return{
+          color:e.isActive?"red":"white",
+           fontWeight:e.isActive?"bold":"normal"
+        }
+      }} to="/about">About</NavLink>
     </nav>
     </div>
   )
