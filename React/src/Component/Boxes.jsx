a9313b87cef407aa533d01763c38fa472c190d5d
anantkpatel <imagine231216@gmail.com>
2025-09-27 10:05:08 +0530

    ok

--- React/src/Component/Boxes.jsx
+++ React/src/Component/Boxes.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import Box from "./Box";
 
 function Boxes({ users }) {
-  console.log("users in Boxes.jsx:", users); // Debugging ke liye
-
-  // ✅ Safety check - agar users undefined ho to empty array use karo
-  // const safeUsers = Array.isArray(users) ? users : [];
+  // Safety check - agar users undefined ho to empty array use karo
+  const safeUsers = Array.isArray(users) ? users : [];
 
   return (
     <div className="w-full h-96 max-h-72 overflow-auto p-5 flex gap-4 justify-center flex-wrap">
-      {users.map((users, index) => {
-       return  <Box key={index}  />
-      }
-       
-      )}
+      {safeUsers.map((user, index) => {
+        return <Box key={index} />;
+      })}
     </div>
   );
 }
