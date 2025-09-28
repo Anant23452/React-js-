03a3a0006c14584c968fc8735ef001b0a951aa3b
anantkpatel <imagine231216@gmail.com>
2025-09-28 10:47:57 +0530

    h

--- ReactRout/my-app/utils/Context.jsx
+++ ReactRout/my-app/utils/Context.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Context(props) {
+    const[user,setUser]=useState([
+        {name:"Aman",email:"aman@gmail"},
+        {name:"Ankit",email:"ankita@gmail"},
+        {name:"Sumit",email:"sumit@gmail"},
+        {name:"Rohit",email:"rohit@gmail"},
+    ])
+
+   const UserContext=React.createContext();
+    
     console.log(props);
-  return (
-    props.children
-  )
+  return <UserContext.Provider value={{users,setUser}}>  { props.children}  </UserContext.Provider>
+   
+  
 }
 
 export default Context
