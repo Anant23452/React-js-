d06a27c885465682786ad76aa5fbebec50735f99
anantkpatel <imagine231216@gmail.com>
2025-09-29 22:29:57 +0530

    t5

--- ReactRout/my-app/Components/Nav.jsx
+++ ReactRout/my-app/Components/Nav.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link, NavLink } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 function Nav() {
   return (
     <div>
-          <nav className="mt-10 flex justify-around bg-black text-white p-5">
-      <NavLink style={(e)=>{
-        return{
-          color:e.isActive?"red":"white",
-           fontWeight:e.isActive?"bold":"normal"
-        }
-      }}  to="/">Home</NavLink>
-      <NavLink style={(e)=>{
-          return{
-          color:e.isActive?"red":"white",
-          fontWeight:e.isActive?"bold":"normal"
-        }
-      }} to="/user">User</NavLink>
-      <NavLink style={(e)=>{
-          return{
-          color:e.isActive?"red":"white",
-           fontWeight:e.isActive?"bold":"normal"
-        }
-      }} to="/about">About</NavLink>
-    </nav>
+      <nav className="mt-10 flex justify-around bg-black text-white p-5">
+        <NavLink
+          style={(e) => {
+            return {
+              color: e.isActive ? "red" : "white",
+              fontWeight: e.isActive ? "bold" : "normal",
+            };
+          }}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          style={(e) => {
+            return {
+              color: e.isActive ? "red" : "white",
+              fontWeight: e.isActive ? "bold" : "normal",
+            };
+          }}
+          to="/user"
+        >
+          User
+        </NavLink>
+        <NavLink
+          style={(e) => {
+            return {
+              color: e.isActive ? "red" : "white",
+              fontWeight: e.isActive ? "bold" : "normal",
+            };
+          }}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink >Students</NavLink>
+
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
--- ReactRout/my-app/utils/Routing.jsx
+++ ReactRout/my-app/utils/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from '../Components/Home'
 import User from '../Components/User'
 import About from '../Components/About'
+import Student from '../Components/Student'
 import { Route, Routes } from 'react-router-dom'
 
 function Routing() {
@@ -10,6 +11,7 @@ function Routing() {
     <Routes>
     <Route path='/' element={<Home/>}></Route>
     <Route path='/user' element={<User/>}></Route>
+    <Route path='/Student' element={<Student/>}></Route>
     <Route path='/about' element={<About/>}></Route>
 
 
